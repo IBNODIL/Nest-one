ffac141ad8dcea700c7f3e4d11aebe74dc4851a9
IBNODIL <ibnodil2010@gmail.com>
2023-11-03 11:35:25 +0300

    add contact with telegram section

--- src/App.vue
+++ src/App.vue
@@ -430,7 +430,34 @@ import { RouterLink, RouterView } from 'vue-router'
 
       </div>
     </div>
+
+    <v-card rounded="xl" class="position-relative mt-16 mb-5 mx-10 d-flex justify-space-between pa-0" style="width: 80%;">
+      <div class="d-flex pa-5 flex-column justify-space-between" style="width: 400px; height: 300px;">
+      <div class="text-h5">
+        To get answers to the questions, contact us now by telegram!
+      </div>
+
+      <div class="d-flex align-center">
+        <v-icon class="mx-10">
+          <img src="./assets/img/operator.svg" alt="">
+        </v-icon>
+        <div>
+          <p class="text-body-1">Anna Alexandra</p>
+          <p class="text-caption">Adminstrator</p>
+        </div>
+      </div>
+
+      <div transition="slide-x-transition" class="ma-6 hero-read-more"> <v-btn size="small" class="callbeck-1" icon="mdi-arrow-top-right"></v-btn> <v-btn rounded="xl" ><v-icon class="mr-2" color="blue"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/></svg></v-icon> Request and call back</v-btn> <v-btn size="small" class="callbeck-2" icon="mdi-arrow-top-right"></v-btn> </div>
+    
+      </div>
+
+      <div>
+        <img height="300" class="pa-3 position-absolute" src="./assets/img/operators.png" alt="">
+      </div>
+    </v-card>
   </div>
+
+  
 </template>
 
 <style scoped>
